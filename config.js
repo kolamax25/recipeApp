29a939e9051f1d3810a09be01ada1a8afae0e8ed
kolamax25 <75153987+kolamax25@users.noreply.github.com>
2021-09-24 19:33:33 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBkZUwL5C3S2MLs5hy86DsezteMMwXLP2s",
+  authDomain: "storyapp-eb4c9.firebaseapp.com",
+  databaseURL: "https://storyapp-eb4c9-default-rtdb.firebaseio.com",
+  projectId: "storyapp-eb4c9",
+  storageBucket: "storyapp-eb4c9.appspot.com",
+  messagingSenderId: "131629885362",
+  appId: "1:131629885362:web:619599cb060d2e2ad226ab"
+};
